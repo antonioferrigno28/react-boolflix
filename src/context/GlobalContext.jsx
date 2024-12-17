ccead9886e30371317f570b5bf9ed3a15850a9a9
antonioferrigno28 <ferrigno1@hotmail.it>
2024-12-17 17:46:18 +0100

    api key url

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -4,9 +4,9 @@ export const GlobalContext = createContext();
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-const URL_movie = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+const URL_movie = `https://api.themoviedb.org/3/search/movie?api_key=d967bf2ba828b9cc05b0e616f645bb10&query=`;
 
-const URL_tvseries = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=`;
+const URL_tvseries = `https://api.themoviedb.org/3/search/tv?api_key=d967bf2ba828b9cc05b0e616f645bb10&query=`;
 
 export const GlobalContextData = ({ children }) => {
   const [movies, setMovies] = useState([]);
